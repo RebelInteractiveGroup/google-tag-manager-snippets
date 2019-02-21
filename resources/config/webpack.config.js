e303ae273a0503fd716aae45c49e5db57c7d7c40
MWDelaney <michael.delaney@gmail.com>
2019-02-21 12:35:12 -0500

    Minify bundle

--- resources/config/webpack.config.js
+++ resources/config/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, '../scripts'),
   entry: {
     app: './app.js',
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        test: /\.js(\?.*)?$/i,
+      }),
+    ],
   },
   output: {
     path: path.resolve(__dirname, '../../dist'),
